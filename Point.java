40d58ed504d0d2b32eacd83f79ab9992221cfa36
Max Vaysburd <max.vays@gmail.com>
2019-04-05 07:35:20 -0400

    Implement compareTo for Point

--- Point.java
+++ Point.java
@@ -4,10 +4,10 @@
 public class Point{
     private double xcor;
     private double ycor;
-    
+
     /**
       @return  a negative integer, zero, or a positive integer
-      depending on whether this Point is closer, 
+      depending on whether this Point is closer,
       equidistant, or farther than the Point referred to
       by the parameter.
      */
@@ -17,7 +17,9 @@ public class Point{
            If the math is a problem, use a simpler
            relationship, like "higher is bigger"
            and change the tests accordingly */
-        return 0; // temp: all Points are One
+        double myDistance = Math.pow((Math.pow(xcor,2) + Math.pow(ycor,2)),(1.0/2.0));
+        double otherDistance = Math.pow((Math.pow(((Point)otherObj).xcor,2) + Math.pow(((Point)otherObj).ycor,2)),(1.0/2.0));
+        return Double.compare(myDistance,otherDistance);
     }
 
     // -------- previously-written code ----------
@@ -26,12 +28,12 @@ public class Point{
         this.xcor = xcor;
         this.ycor = ycor;
     }
-    
+
     /**
       @return a string representation of this instance
      */
     public String toString() {
-        return "(" + xcor + "," + ycor + ")"; 
+        return "(" + xcor + "," + ycor + ")";
     }
 
 }
--- UserOfComparability.java
+++ UserOfComparability.java
@@ -12,19 +12,19 @@ public class UserOfComparability {
         Point p0 = new Point( 3., 4.);
         reportRelationship( "Point vs. itself", p0, p0, "0");
 
-        // // Point vs. its reflection about y = x
-        // reportRelationship( "Point vs. its reflection about y = x"
-                          // , p0, new Point( 4., 3.), "0");
+        // Point vs. its reflection about y = x
+        reportRelationship( "Point vs. its reflection about y = x"
+                          , p0, new Point( 4., 3.), "0");
 
-        // // Point vs. point closer to origin
-        // reportRelationship( "Point vs. point closer to origin", p0
-                          // , new Point( 1., 2.), "positive int");
+        // Point vs. point closer to origin
+        reportRelationship( "Point vs. point closer to origin", p0
+                          , new Point( 1., 2.), "positive int");
 
-        // // Point vs. distant point
-        // reportRelationship( "Point vs. distant point", p0
-                          // , new Point( 100., 2.), "negative int");
-        // System.out.println( "---- end of Point comparisons ----");
-        
+        // Point vs. distant point
+        reportRelationship( "Point vs. distant point", p0
+                          , new Point( 100., 2.), "negative int");
+        System.out.println( "---- end of Point comparisons ----");
+
         // System.out.println( System.lineSeparator()
                           // + "Date comparisons");
     }
@@ -33,7 +33,7 @@ public class UserOfComparability {
     /**
       Print the results of comparing two entities.
      */
-    private static void reportRelationship 
+    private static void reportRelationship
       ( String description
       , Point a
       , Point b
@@ -41,16 +41,16 @@ public class UserOfComparability {
       ) {
         System.out.println( description);
 
-        // check 
+        // check
         System.out.println(
             a + " compareTo " + b + ": "
           + a.compareTo( b)
-            /* eventually expecting a compiler warning: 
+            /* eventually expecting a compiler warning:
               warning: [unchecked] unchecked call to compareTo(T) as a member of the raw type Comparable
               where T is a type-variable:
                 T extends Object declared in interface Comparable
              */
-          
+
           + " ...expecting " + expect
           + System.lineSeparator());
      }
